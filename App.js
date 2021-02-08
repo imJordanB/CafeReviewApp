bf49813540790892749b2417f87c6a032d6f2451
Jordan Burns <ijtbservices@protonmail.com>
2021-02-08 16:53:33 +0000

    feat(App.js): Add logo, email and password inputs, sign in/sign up button and styles

--- App.js
+++ App.js
@@ -1,57 +1,91 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { Text, TextInput, View, Button, StyleSheet, Alert } from 'react-native';
+import { Text, TextInput, View, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 class App extends Component{
   constructor(props){
     super(props);
   }
 
+  state={
+    email: "",
+    paassword: ""
+  }
+
   render(){
     return (
-      <></>
+      <View style={styles.container}>
+        <Text style={styles.logo}>COFFIDA</Text>
+        <View style={styles.inputView}>
+          <TextInput 
+          style={styles.inputText}
+          placeholder="Email address"
+          placeholderTextColor="#FFF"
+          onChangeText={text => this.setState({email:text})}/>
+        </View>
+
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Password"
+            placeholderTextColor="#FFF"
+            secureTextEntry={true}
+            onChangeText={text => this.setState({password:text})}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     );
   };
 }
- 
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  logo:{
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40
   },
-  body: {
-    backgroundColor: Colors.white,
+  inputView:{
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  inputText:{
+    height: 50,
+    color: "white"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  loginText:{
+    color: "#FFF"
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  loginBtn:{
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 5,
+    marginBottom: 10
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+})
 
 export default App;
